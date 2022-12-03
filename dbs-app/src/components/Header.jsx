23270b70392798060d613ec98b31ba32ea8a013c
Yu Rui Chua <jchuayurui@gmail.com>
2022-12-03 12:11:34 +0800

    test
    design changes
    
    design changes 2
    
    design changes 3
    
    added register

--- dbs-app/src/components/Header.jsx
+++ dbs-app/src/components/Header.jsx
@@ -14,8 +14,8 @@ function Header() {
     return <header className="bd-header bg-dark py-3 d-flex align-items-stretch border-bottom border-dark">
     <div className="container-fluid d-flex align-items-center">
       <h1 className="d-flex align-items-center fs-4 text-white mb-0">
-        <img src="logo.svg" width="38" height="30" className="me-3" alt="Bootstrap"/>
-        Cheatsheet
+        <img src="logo.png"  height="60" className="me-3" alt="DBS"/>
+        Digibank
       </h1>
       <a href="/" onClick={Signout} className="ms-auto link-light" hrefLang="ar">Sign out</a>
     </div>
---
+++ dbs-app/src/components/Login.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import setAuthToken from "../setAuthToken"
+import Header from './Header';
+const shadstyle= `
+1px 2px 2px hsl(220deg 30% 50% / 0.333),
+2px 4px 4px hsl(220deg 30% 50% / 0.333),
+3px 6px 6px hsl(220deg 30% 50% / 0.333)`
+const boxshadow={
+    "height" : "50vh",
+    "width": "50vh",
+    "box-shadow": shadstyle,
+    "position": "center",
+    
+
+}
+function Login() {
+  const navigate = useNavigate();
+  const [msg, setMsg] = React.useState('');
+  const [auth, setAuth] = React.useState({
+    username: "",
+    password: ""
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setAuth(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+        await axios.post('http://localhost:3001/api/auth/login', {
+            username: auth.username,
+            password: auth.password
+        })
+        .then(response=>{
+          if (!response.data.accessToken){
+            navigate("/login")
+          }
+          else {
+          const token = response.data.accessToken;
+          localStorage.setItem("token", token);
+          setAuthToken(token);
+          }
+        });
+        
+    } catch (error) {
+        if (error.response) {
+            setMsg(error.response.data.msg);
+        }
+    } finally {
+      navigate("/");
+    }
+}
+    return (<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', width: '100vw' ,height: '100vh', backgroundImage: `url("https://planetofhotels.com/guide/sites/default/files/styles/paragraph__hero_banner__hb_image__1880bp/public/hero_banner/Gardens-by-the-Bay.jpg")`}}>
+      <Header/>
+        <main className=" form-signin container-fluid">
+          <div className="card card-body mx-auto" style={boxshadow} >
+        <form >
+          <div className="container-fluid mx-auto" style={{'align-items':"center", justifyContent:'center'}}>
+        <img src="desktoplogo.png" className="img-responsive center-block mx-auto"/>
+        </div>
+        <br/>
+        <br/>
+          <h1 class="h6 mb-3 fw-normal">Sign in to digibank</h1>
+          
+          
+          <div class="form-floating">
+            <input type="username" class="form-control" id="floatingInput" onChange={handleChange} name="username" placeholder="name@example.com"/>
+            <label for="floatingInput">Username</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" onChange={handleChange} name="password" />
+            <label for="floatingPassword">Password</label>
+          </div>
+      
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"/> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" onClick={login} type="submit">Sign in</button>
+          <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+        </form>
+        </div>
+      </main>
+      </div>)
+}
+
+
+export default Login;
