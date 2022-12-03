fff1795ed861928716e56973c0e8d291367a3a09
limjiewwei <jiewwei@hotmail.com>
2022-12-03 16:57:09 +0800

    updated profilepage.js with back

--- dbs-app/src/pages/Profilepage.js
+++ dbs-app/src/pages/Profilepage.js
@@ -1,71 +1,68 @@
-import '../App.css';
-import { useEffect, useState, useContext } from 'react'
-import { useNavigate } from'react-router-dom'
-import axios from 'axios'
-import Button from 'react-bootstrap/Button';
+import "../App.css";
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
 
 const Profilepage = () => {
-    const navigate = useNavigate()
-    const [userObject, setUserObject] = useState({})
-    const [address, setAddress] = useState("")
-    const [email, setEmail] = useState("")
-    const [checkBox, setCheckBox] = useState("")
-    const backButton = () => {
-      navigate(-1);
-    };
+	const navigate = useNavigate();
+	const [userObject, setUserObject] = useState({});
+	const [address, setAddress] = useState("");
+	const [email, setEmail] = useState("");
+	const [checkBox, setCheckBox] = useState("");
 
-    useEffect(() => {
-      // axios.get(`http://localhost:3001/accounts/${id}`).then((response) => {
-      //   setUserObject(response.data)
-      // })
-      if (userObject.OptIntoPhyStatements == 1) {
-        setCheckBox("checked")
-      }
-    }, [])
+	useEffect(() => {
+		// axios.get(`http://localhost:3001/accounts/${id}`).then((response) => {
+		//   setUserObject(response.data)
+		// })
+		if (userObject.OptIntoPhyStatements == 1) {
+			setCheckBox("checked");
+		}
+	}, []);
 
-    const cancel = () => {
-      navigate("/")
-    }
+	const cancel = () => {
+		navigate("/dashboard");
+	};
 
-    const edit = () => {
-      
-    }
+	const edit = () => {};
 
-  return (
-    <div>
-      <Button variant="outline-secondary" onClick={backButton}>Back</Button>
-    <form class="profileContainer">
-    <h3>Profile Page</h3>
-    <div class="form-group">
-      <label>Username</label>
-      <input disabled type="text" class="form-control" />
-    </div>
-    <div class="form-group">
-      <label>First Name</label>
-      <input disabled type="text" class="form-control" />
-    </div>
-    <div class="form-group">
-      <label>Last Name</label>
-      <input disabled type="text" class="form-control" />
-    </div>
-    <div class="form-group">
-      <label for="address">Address</label>
-      <input type="passtextword" class="form-control" id="address" />
-    </div>
-    <div class="form-group">
-      <label for="email">Email</label>
-      <input type="text" class="form-control" id="email" />
-    </div>
-    <div class="form-check">
-      <input disabled type="checkbox" class="form-check-input" id="checkBox" checked={checkBox} />
-      <label class="form-check-label">Opt in Physical Statements</label>
-    </div>
-    <button onClick={edit} type="submit" class="btn btn-primary">Save</button>
-    <button onClick={cancel} type="submit" class="btn btn-primary">Cancel</button>
+	return (
+		<div>
+			<form class="profileContainer">
+				<h3>Profile Page</h3>
+				<div class="form-group">
+					<label>Username</label>
+					<input disabled type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>First Name</label>
+					<input disabled type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>Last Name</label>
+					<input disabled type="text" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label for="address">Address</label>
+					<input type="passtextword" class="form-control" id="address" />
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" class="form-control" id="email" />
+				</div>
+				<div class="form-check">
+					<input disabled type="checkbox" class="form-check-input" id="checkBox" checked={checkBox} />
+					<label class="form-check-label">Opt in Physical Statements</label>
+				</div>
+				<button onClick={edit} type="submit" class="btn btn-primary">
+					Save
+				</button>
+				<button onClick={cancel} type="submit" class="btn btn-primary">
+					Cancel
+				</button>
+			</form>
+		</div>
+	);
+};
 
-    </form>
-    </div>
-  )
-}
-
-export default Profilepage
+export default Profilepage;
