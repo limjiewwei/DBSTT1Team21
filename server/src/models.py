14c98bc829f6ba62d43b5bfcac79347639df6976
viceroi <107919050+viceroi@users.noreply.github.com>
2022-12-03 13:25:03 +0800

    Updated Models.py
    Created models for SQLAlchemy

--- server/src/models.py
+++ server/src/models.py
@@ -6,14 +6,17 @@ from uuid import uuid4
 
 db = SQLAlchemy()
 
+
 def get_uuid():
     return uuid4().hex
 
+
 # database schema models
+
 
 class User(db.Model):
     __tablename__ = "User"
-    UserID = db.Column(db.Integer, default=get_uuid, primary_key=True)
+    UserID = db.Column(db.Integer, primary_key=True)
     # userID generated with UUID
     UserName = db.Column(db.String(20), unique=True, nullable=False)
     Password = db.Column(db.String(20), unique=False, nullable=False)
@@ -21,7 +24,24 @@ class User(db.Model):
     LastName = db.Column(db.String(255), unique=False, nullable=False)
     Email = db.Column(db.String(255), unique=False)
     Address = db.Column(db.String(255), unique=False)
-    OptIntoPhyStatements =db.Column(db.Integer(1), unique=False), nullable=False)
-   
+    OptIntoPhyStatements = db.Column(db.Integer(1), unique=False)
 
 
+class Bank_Account(db.Model):
+    __tablename__ = "Bank_Account"
+    AccountID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, db.ForeignKey("User.UserID"), nullable=False)
+    AccountType = db.Column(db.String(255), unique=False, nullable=False)
+    AccountBalance = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+
+
+class Scheduled_Transaction(db.Model):
+    __tablename__ = "Scheduled_Transaction"
+    TransactionID = db.Column(db.Integeer, primary_key=True)
+    AccountID = db.Column(
+        db.Integer, db.ForeignKey("Bank_Account.AccountID"), nullable=False
+    )
+    ReceivingAccountID = db.Column(db.Integer(255), unique=False, nullable=False)
+    Date = db.Column(db.String(255), nullable=False)
+    TransactionAmount = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    Comment = db.Column(db.String(255), nullable=False)
