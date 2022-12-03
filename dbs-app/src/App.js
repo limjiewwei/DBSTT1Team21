59113e93cd5ed95707edabb67ab3ce69ebd0d3f0
limjiewwei <jiewwei@hotmail.com>
2022-12-03 12:10:38 +0800

    updated layout

--- dbs-app/src/App.js
+++ dbs-app/src/App.js
@@ -1,23 +1,24 @@
 import MainMenu from "./components/MainMenu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import PrivateRoutes from "./RouteGuard";
+// import Header from "./components/Header";
+// import Home from "./components/Home";
+// import Register from "./components/Register";
+// import Login from "./components/Login";
+// import PrivateRoutes from "./RouteGuard";
 
 function App() {
 	return (
 		<div className="App">
-			<Header />
+			<MainMenu />
+			{/* <Header />
 			<Routes>
 				<Route element={<PrivateRoutes />}>
 					<Route path="/" element={<Home />} />
 				</Route>
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
-			</Routes>
+			</Routes> */}
 		</div>
 	);
 }
