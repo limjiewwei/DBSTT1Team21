d67a3f90c6286231f6355d99f6e719f50d368f94
Yu Rui Chua <jchuayurui@gmail.com>
2022-12-03 14:45:08 +0800

    merge compatibility

--- dbs-app/src/App.js
+++ dbs-app/src/App.js
@@ -1,72 +1,17 @@
-/*import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainMenu from "./components/MainMenu";
+import { Navigate, useLocation } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const location = useLocation();
+	return (
+		<>
+			{location.pathname !== "/login" && <MainMenu />}
+			<Outlet />
+		</>
+	);
+};
 
-export default App;
-*/
-import React, { Component } from 'react';
-import { Route, Routes } from "react-router-dom"
-import './App.css';
-import Header from  './components/Header'
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login'
-import PrivateRoutes from './RouteGuard'
-function App() {
-  return (
-    /*
-    <div className='container'>
-    <Routes>
-               <Route
-                   exact
-                   path="/"
-                   element={Home}
-               />
-               <Route
-                   path="/register"
-                   element={Register}
-               />
-               <redirect to="/" />
-           </Routes>
-           </div>
-           */
-           /*
-            <Router>
-              <Routes>
-                <Route path="/" element={<Home />} />
-              </Routes>
-            </Router>*/<>
-            <Header/>
-              <Routes>
-                <Route element={<PrivateRoutes/>}>
-                  <Route path="/" element={<Home />} />
-                </Route>
-                
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />}/>
-              </Routes>
-              </>
-          
-  );
-}
 export default App;
