b19b4c8793b0ed6f00d6c55ce0d06cb95c2cb266
Yu Rui Chua <jchuayurui@gmail.com>
2022-12-03 15:59:50 +0800

    added routeguard

--- dbs-app/src/index.js
+++ dbs-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
+import PrivateRoutes from './RouteGuard'
 import Login from "./pages/Login";
 import Dashboard from "./components/DashboardOverview";
 import NewTransactions from "./pages/NewTransactions";
@@ -11,8 +12,10 @@ render(
 	<Router>
 		<Routes>
 			<Route path="/" element={<App />}>
-				<Route path="dashboard" element={<Dashboard />} />
+				
+				
 				<Route path="login" element={<Login />} />
+				<Route element={<PrivateRoutes/>}>
 				<Route
 					path="*"
 					element={
@@ -21,8 +24,10 @@ render(
 						</main>
 					}
 				/>
+				<Route path="dashboard" element={<Dashboard />} />
 				<Route path="profilepage" element={<Profilepage />} />
 				<Route path="newtransaction" element={<NewTransactions />} />
+				</Route>
 			</Route>
 		</Routes>
 	</Router>,
