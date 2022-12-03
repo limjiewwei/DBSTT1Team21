222ae722a4e51caa3e5624b362e6516cfac039f4
Yu Rui Chua <jchuayurui@gmail.com>
2022-12-03 11:50:35 +0800

    routeguard

--- dbs-app/src/App.js
+++ dbs-app/src/App.js
@@ -1,20 +1,72 @@
-import logo from "./logo.svg";
-import "./App.css";
+/*import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
+export default App;
+*/
+import React, { Component } from 'react';
+import { Route, Routes } from "react-router-dom"
+import './App.css';
+import Header from  './components/Header'
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login'
+import PrivateRoutes from './RouteGuard'
+function App() {
+  return (
+    /*
+    <div className='container'>
+    <Routes>
+               <Route
+                   exact
+                   path="/"
+                   element={Home}
+               />
+               <Route
+                   path="/register"
+                   element={Register}
+               />
+               <redirect to="/" />
+           </Routes>
+           </div>
+           */
+           /*
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Router>*/
+            <><Header/>
+              <Routes>
+                <Route element={<PrivateRoutes/>}>
+                  <Route path="/" element={<Home />} />
+                </Route>
+                
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />}/>
+              </Routes>
+              </>
+          
+  );
+}
 export default App;
--- dbs-app/src/index.js
+++ dbs-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +15,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+
+render((
+    <Router>
+        <App/>
+    </Router>
+), document.getElementById('root'));
