c4344c05041500c0251d7aba0655d71d6d0e66a0
lohpeishan97 <119652866+lohpeishan97@users.noreply.github.com>
2022-12-03 16:16:57 +0800

    added API (testing)

--- dbs-app/src/pages/Profilepage.js
+++ dbs-app/src/pages/Profilepage.js
@@ -23,8 +23,14 @@ const Profilepage = () => {
       navigate("/")
     }
 
-    const edit = () => {
-      
+    const edit = (id) => {
+      // axios.put("http://localhost:3001/accounts/transfer",
+      // {
+      //   address: address,
+      //   email: email,
+      //   id: id
+      // },
+      setUserObject({...userObject, address: address})
     }
 
   return (
@@ -32,29 +38,29 @@ const Profilepage = () => {
     <h3>Profile Page</h3>
     <div class="form-group">
       <label>Username</label>
-      <input disabled type="text" class="form-control" />
+      <input disabled type="text" value="" class="form-control" />
     </div>
     <div class="form-group">
       <label>First Name</label>
-      <input disabled type="text" class="form-control" />
+      <input disabled type="text" value="" class="form-control" />
     </div>
     <div class="form-group">
       <label>Last Name</label>
-      <input disabled type="text" class="form-control" />
+      <input disabled type="text" value="" class="form-control" />
     </div>
     <div class="form-group">
       <label for="address">Address</label>
-      <input type="passtextword" class="form-control" id="address" />
+      <input value={address} onChange={(e) => {setAddress(e.target.value)}} type="passtextword" class="form-control" id="address" />
     </div>
     <div class="form-group">
       <label for="email">Email</label>
-      <input type="text" class="form-control" id="email" />
+      <input value={email} onChange={(e) => {setEmail(e.target.value)}} type="text" class="form-control" id="email" />
     </div>
     <div class="form-check">
       <input disabled type="checkbox" class="form-check-input" id="checkBox" checked={checkBox} />
       <label class="form-check-label">Opt in Physical Statements</label>
     </div>
-    <button onClick={edit} type="submit" class="btn btn-primary">Save</button>
+    <button onClick={() => {edit(userObject.id)}} type="submit" class="btn btn-primary">Save</button>
     <button onClick={cancel} type="submit" class="btn btn-primary">Cancel</button>
 
     </form>
