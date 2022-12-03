29d3c2a0ae67f3400084b93e8040c4b5f5a4ac83
Yu Rui Chua <jchuayurui@gmail.com>
2022-12-03 12:11:34 +0800

    test

---
+++ dbs-app/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Body from "./Body";
+import Header from "./Header"
+function Home(){
+    return <div>
+        <Header/>
+        <Body/>
+    </div>
+}
+export default Home;
